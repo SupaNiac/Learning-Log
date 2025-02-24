e9ab2779acf2be179626e662c6e668784cadb3d7
Luiz Henrique <luiz04faria@gmail.com>
2025-02-24 18:43:24 -0300

    Created class, methods and etc.

--- StudentsRegistration/src/entities/Student.java
+++ StudentsRegistration/src/entities/Student.java
@@ -2,4 +2,49 @@ package entities;
 
 public class Student {
 
+		String name;
+		Integer age;
+		Double grades;
+		
+		
+		public Student (String name, Integer age, Double grades) {
+			this.name = name;
+			this.age = age;
+			this.grades = grades;
+		}
+
+
+		public String getName() {
+			return name;
+		}
+
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+
+		public Integer getAge() {
+			return age;
+		}
+
+
+		public void setAge(Integer age) {
+			this.age = age;
+		}
+
+
+		public Double getGrades() {
+			return grades;
+		}
+
+
+		public void setGrades(Double grades) {
+			this.grades = grades;
+		}
+		
+		public double avarageScore(Double grades) {
+			return grades + grades + grades / 3;
+		}
+		
 }
