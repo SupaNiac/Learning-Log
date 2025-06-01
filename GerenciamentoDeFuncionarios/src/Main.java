cd1381238b83996790ed0bcc10f2c6679a68f979
Luiz Henrique <luiz04faria@gmail.com>
2025-06-01 01:03:01 -0300

    All numbers finished, toString added. Code is now completed!

--- GerenciamentoDeFuncionarios/src/Main.java
+++ GerenciamentoDeFuncionarios/src/Main.java
@@ -9,12 +9,14 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<Funcionario> list = new ArrayList<>();
 
+        int res = 0;
+
+        while (res != 5) {
             System.out.println(" 1 - Adicionar, 2 - Remover, 3 - Buscar, 4 - Listar, 5 - Sair.");
             System.out.print("Digite o que você deseja fazer: ");
-            int res = sc.nextInt();
-
-            ArrayList<Funcionario> list = new ArrayList<>();
+            res = sc.nextInt();
 
             if (res == 1) {
                 System.out.print("Nome: ");
@@ -28,37 +30,36 @@ public class Main {
                 Funcionario funcionario = new Funcionario(nome, id, salario, cargo);
                 list.add(funcionario);
                 System.out.println("Funcionário adicionado com sucesso!");
-            }
-            else if  (res == 2){
+
+            } else if (res == 2) {
                 System.out.print("Digite o ID do funcionario que você deseja remover: ");
                 int id = sc.nextInt();
-                for (int i = 0; i<list.size(); i++) {
+                for (int i = 0; i < list.size(); i++) {
                     Funcionario f = list.get(i);
-                    if (f.getID() == id){
+                    if (f.getID() == id) {
                         list.remove(f);
                         System.out.println("Funcionário removido com sucesso!");
                         break;
                     }
                 }
 
-            }
-            else if (res == 3) {
+            } else if (res == 3) {
                 System.out.print("Digite o ID do funcionario: ");
                 int id = sc.nextInt();
-                for (int i = 0; i<list.size(); i++) {
+                for (int i = 0; i < list.size(); i++) {
                     Funcionario f = list.get(i);
-                    if (f.getID() == id){
-                        System.out.println("Funcionário: " + f.getNome() + "Cargo: " + f.getCargo() + "Salário: " + f.getSalario() + "Id: " + f.getID());
+                    if (f.getID() == id) {
+                        System.out.println("Funcionário: " + f.getNome() + " | Cargo: " + f.getCargo() + " | Salário: " + f.getSalario() + " | Id: " + f.getID());
                         break;
                     }
                 }
+            } else if (res == 4) {
+                for (Funcionario f : list) {
+                    System.out.println(f);
+                }
             }
-            else if (res == 4) {
-                System.out.println("Listando... ");
 
-            }
-
-
+        }
 
         }
     }
--- GerenciamentoDeFuncionarios/src/entities/Funcionario.java
+++ GerenciamentoDeFuncionarios/src/entities/Funcionario.java
@@ -4,8 +4,8 @@ public class Funcionario {
 
     String Nome;
     Integer ID;
-    String Cargo;
     Double Salario;
+    String Cargo;
 
     public Funcionario(String nome, Integer ID, Double salario, String cargo) {
         Nome = nome;
@@ -28,6 +28,14 @@ public class Funcionario {
 
     public Double getSalario() {
         return Salario;
+    }
+
+    @Override
+    public String toString(){
+        return "ID: " + this.ID + "\n" +
+                 "Nome: " + this.Nome + "\n" +
+                 "Salário: " + this.Salario + "\n" +
+                 "Cargo: " + this.Cargo + "\n";
     }
 
 }
